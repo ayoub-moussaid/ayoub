96550a0ecf51acb78c30d1ce200ec2c80012f713
ayoub-moussaid <ayoublofi15@gmail.com>
2025-04-21 15:01:28 +0100

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,12 @@
+l1=[]
+
+for i in range(0,10):
+    x= int(input("entrer un nombre:"))
+    l1.append(x)
+print(l1)
+max=max(l1)
+min=min(l1)
+print("le min est:",min)
+print("le max est:",max)
+
+    
